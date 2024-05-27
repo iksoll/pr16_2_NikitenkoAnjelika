9b8bcd5f51d395184d62c7db90d53396daf64ca6
iksoll <157449149+iksoll@users.noreply.github.com>
2024-05-27 10:37:27 +0500

    Add files via upload

---
+++ prakt16/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] a = { 5.1, 1.3, 9.2, 2.3, 5.1, 3 };
+            Dictionary<double, int> frequencies = new Dictionary<double, int>();
+            foreach (double num in a)
+                    {
+                if (frequencies.ContainsKey(num))
+                {
+                    frequencies[num]++;
+                }
+                else
+                {
+                    frequencies[num] = 1;
+                }
+            }
+            Console.WriteLine("Старый массив");
+            foreach (var pair in frequencies)
+            {
+                Console.WriteLine("Число: " + pair.Key);
+                Console.WriteLine("Частота: " + pair.Value);
+                Console.WriteLine();
+                for (int i=0;i<a.Length; i++)
+                {
+                    if (a[i] == pair.Key)
+                    {
+                        a[i] = a[i] * pair.Value;
+                    }
+                }
+            }
+            Console.WriteLine("Новый массив");
+            foreach (double num in a)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
